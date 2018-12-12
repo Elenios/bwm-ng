9fcb99deb0531e26a7760359e5d530bb89824382
Elenios <gabor.dobrozemszky@hotmail.com>
2018-12-12 13:19:28 +0100

    Update(RentalModule): implement into AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,9 @@ import { HeaderComponent } from './common/header/header.component';
 import { RentalComponent } from './rental/rental.component';
 import { TempComponent } from './temp/temp.component';
 
-import { RentalListComponent } from './rental/rental-list/rental-list.component'
-import { RentalListItemComponent } from './rental/rental-list-item/rental-list-item.component'
+import { RentalModule } from './rental/rental.module'
 
-
-const routes = [
+const routes: Routes = [
   { path: '', component: RentalComponent },
   { path: 'temp', component: TempComponent }
 ]
@@ -20,14 +18,12 @@ const routes = [
   declarations: [
     AppComponent,
     HeaderComponent,
-    RentalComponent,
     TempComponent,
-    RentalListComponent,
-    RentalListItemComponent 
   ],
   imports: [
     RouterModule.forRoot(routes),
-    BrowserModule
+    BrowserModule,
+    RentalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
