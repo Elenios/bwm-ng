7bddde9047fdac8e3ebf99f1e845cbdf60828bc5
Gabor Dobrozemszky <gabordobrozemszky@Gabors-MacBook-Pro.local>
2018-12-15 21:52:06 +0100

    add: auth process logic

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,8 +1,33 @@
 const User = require('../models/user');
-const { normalizeErrors } = require('../helpers/mongoose')
+const { normalizeErrors } = require('../helpers/mongoose');
+const jwt = require('jsonwebtoken');
+const config = require('../config/dev');
 
 exports.auth = function(req, res) {
+  const { email, password } = req.body;
 
+  if (!email || !password) {
+    return res.status(422).send({ errors: [{ title: 'Data missing!', detail: 'Provide email and password' }] });
+  }
+
+  User.findOne({ email }, function(err, user) {
+    if (err) {
+      return res.status(422).send({ errors: normalizeErrors(err.errors) });
+    }
+    if (!user) {
+      return res.status(422).send({ errors: [{ title: 'Invalid user!', detail: 'User doesn\'t exist' }] });
+    }
+    if (user.hasSamePassword(password)) {
+      const token = jwt.sign({
+        userID: user.id,
+        username: user.username
+      }, config.SECRET, { expiresIn: '1h' });
+      return res.json(token);
+    } else {
+      return res.status(422).send({ errors: [{ title: 'Invalid data!', detail: 'Invalid email or password' }] });
+
+    }
+ });
 }
 
 exports.register = function(req, res) {
