534ef383af1e6715b0a6404eabfb06fd86e89fea
Gabor Dobrozemszky <gabordobrozemszky@Gabors-MacBook-Pro.local>
2018-12-16 23:45:44 +0100

    add LoginComponent: form, validators, errorhandling

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'bwm-login',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required,
+                   Validators.pattern('^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$')]],
+      password: ['', Validators.required]
+    });
+  }
+
+  isInvalidInput(field): boolean {
+    return this.loginForm.controls[field].invalid &&
+          (this.loginForm.controls[field].dirty || this.loginForm.controls[field].touched);
+  }
+
+  isRequired(field): boolean {
+    return this.loginForm.controls[field].errors.required;
+  }
+
+  login() {
+    console.log(this.loginForm.value);
   }
 
 }
